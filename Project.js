438202c58b34335e75996f576d4a5c7024e55b4f
Omar <olr7742@nyu.edu>
2022-07-26 00:13:37 +0300

    Project 3

---
+++ Project.js
@@ -0,0 +1,11 @@
+export class Project {
+    constructor(title, date, image, shortInfo, longInfo) {
+      this.title = title;
+      this.date = date;
+      this.image = image;
+      this.shortInfo = shortInfo;
+      this.longInfo = longInfo;
+    }
+  }
+
+  
