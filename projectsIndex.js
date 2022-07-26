c8f70c9316b037ca54cdd7140a7335544e51fa21
Omar <olr7742@nyu.edu>
2022-07-26 14:15:24 +0300

    katib-lebsi-fineday

--- projectsIndex.js
+++ projectsIndex.js
@@ -1,12 +1,37 @@
-import {Project} from './Project';
+class Project {
+    constructor(title, date, image, shortInfo, longInfo) {
+      this.title = title;
+      this.date = date;
+      this.image = image;
+      this.shortInfo = shortInfo;
+      this.longInfo = longInfo;
+    }
+  }
 
-var lebsiProject = new Project("LEBSI", Date(2021, 9, 1), "lebsi.JPG", "LEBSI is a platform that allows people and clothing brands to set up online stores and list their clothing pieces in minutes. It prevents sellers from having to manage their stores on multiple social media platforms at the same time. LEBSI aims to help physical stores compete with online brands by providing an online marketplace for them to list their items on.", "long info");
-var katibProject = new Project("KATIB", Date(2022, 2, 1), "katib.JPG", "A handwriting assisting platform that provides contact-less kinesthetic feedback by utilizing magnetic forces. KATIB aims to help post-stroke patients regain writing skills.", "long info");
-var qatraProject = new Project("Qatra", Date(2021, 6, 1), "qatra.JPG", "Qatra is a platform that connects those who are in need of blood units in Jordan with potential blood donors.", "long info");
-var fineDayProject = new Project("A Fine Day", Date(2021, 9, 1), "fineDay.JPG", "\"A Fine Day\"is a gamified version of stroke rehabilitation that allows patients to regain their limbs functionality in an engaging environment and therapists to remotely track patient progress", "long info");
-
-var projects = [lebsiProject, katibProject, qatraProject, fineDayProject];
-
-function loadProjects(){
+var lebsiProject = new Project(
+    "LEBSI",
+    Date(2021, 9, 1),
+    "lebsi.JPG", "LEBSI is a platform that allows people and clothing brands to set up online stores and list their clothing pieces in minutes. It prevents sellers from having to manage their stores on multiple social media platforms at the same time. LEBSI aims to help physical stores compete with online brands by providing an online marketplace for them to list their items on.",
+    "long info");
     
-}
+var katibProject = new Project(
+    "KATIB",
+    Date(2022, 2, 1),
+    "katib.JPG",
+    "A handwriting assisting platform that provides contact-less kinesthetic feedback by utilizing magnetic forces. KATIB aims to help post-stroke patients regain writing skills.",
+    "long info");
+
+    var qatraProject = new Project(
+    "Qatra",
+    Date(2021, 6, 1),
+    "qatra.JPG",
+    "Qatra is a platform that connects those who are in need of blood units in Jordan with potential blood donors.",
+    "long info");
+
+var fineDayProject = new Project(
+    "A Fine Day",
+    Date(2021, 9, 1),
+    "fineDay.JPG",
+    "\"A Fine Day\"is a gamified version of stroke rehabilitation that allows patients to regain their limbs functionality in an engaging environment and therapists to remotely track patient progress",
+    "long info");
+
