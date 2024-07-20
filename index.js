77aa2b9a1ebab75aba2af012ce12d18a967f45d9
Omar <olr7742@nyu.edu>
2024-07-20 13:23:25 +0300

    Update

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-window.addEventListener('resize', equalHeightFix);
+window.addEventListener("resize", equalHeightFix);
 
-function equalHeightFix(){
+function equalHeightFix() {
   var w = document.documentElement.clientWidth;
-  if (w>991){
-    console.log("before")
-  var height = this.document.getElementById("secondCol").getBoundingClientRect().height;
-  changeHeights(["firstCol", "thirdCol"], height);
-  document.getElementById("ViewMoreProjects").classList.add("uk-hidden");
-  document.getElementById("ViewMoreProjectsTop").classList.remove("uk-hidden");
-  document.getElementById("ViewMoreExperiencesTop").classList.remove("uk-hidden");
-  document.getElementById("ViewMoreParticipationsTop").classList.remove("uk-hidden");
-}
-  else{
+  if (w > 991) {
+    console.log("before");
+    var height = this.document
+      .getElementById("secondCol")
+      .getBoundingClientRect().height;
+    changeHeights(["firstCol", "thirdCol"], height);
+    document.getElementById("ViewMoreProjects").classList.add("uk-hidden");
+    document
+      .getElementById("ViewMoreProjectsTop")
+      .classList.remove("uk-hidden");
+    document
+      .getElementById("ViewMoreExperiencesTop")
+      .classList.remove("uk-hidden");
+    document.getElementById("ViewMoreUpdatesTop").classList.remove("uk-hidden");
+  } else {
     changeHeights(["firstCol", "thirdCol"], "auto");
     document.getElementById("ViewMoreProjects").classList.remove("uk-hidden");
     document.getElementById("ViewMoreProjectsTop").classList.add("uk-hidden");
-    document.getElementById("ViewMoreExperiencesTop").classList.add("uk-hidden");
-    document.getElementById("ViewMoreParticipationsTop").classList.add("uk-hidden");
+    document
+      .getElementById("ViewMoreExperiencesTop")
+      .classList.add("uk-hidden");
+    document.getElementById("ViewMoreUpdatesTop").classList.add("uk-hidden");
   }
 }
 
-function changeHeights(ids, height){
-  for(let i = 0; i<ids.length; i++){
-    this.document.getElementById(ids[i]).setAttribute("style","height:" + height + "px;text-align:left");
+function changeHeights(ids, height) {
+  for (let i = 0; i < ids.length; i++) {
+    this.document
+      .getElementById(ids[i])
+      .setAttribute("style", "height:" + height + "px;text-align:left");
   }
 }
 
@@ -38,12 +47,12 @@ function scrollToSection(event) {
 function supportsSmoothScrolling() {
   const body = document.body;
   const scrollSave = body.style.scrollBehavior;
-  body.style.scrollBehavior = 'smooth';
-  const hasSmooth = getComputedStyle(body).scrollBehavior === 'smooth';
+  body.style.scrollBehavior = "smooth";
+  const hasSmooth = getComputedStyle(body).scrollBehavior === "smooth";
   body.style.scrollBehavior = scrollSave;
   return hasSmooth;
-};
- 
+}
+
 function SmoothVerticalScrolling(element, time, position) {
   var eTop = element.getBoundingClientRect().top;
   var eAmt = eTop / 100;
@@ -55,15 +64,13 @@ function SmoothVerticalScrolling(element, time, position) {
 }
 
 function SVS_B(eAmt, position) {
-  if (position == "center" || position == "")
-  window.scrollBy(0, eAmt / 2);
-  if (position == "top")
-  window.scrollBy(0, eAmt);
+  if (position == "center" || position == "") window.scrollBy(0, eAmt / 2);
+  if (position == "top") window.scrollBy(0, eAmt);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('show-more').addEventListener('click', function() {
-      document.getElementById('more-items').style.display = 'inline';
-      this.style.display = 'none';
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("show-more").addEventListener("click", function () {
+    document.getElementById("more-items").style.display = "inline";
+    this.style.display = "none";
   });
 });
